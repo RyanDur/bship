818f4f0801e6f59eec3d3abe2d8d1cb6c8d106e4
RyanDur <ryan.p.durling@gmail.com>
2016-11-30 17:30:26 -0600

    remove unused

--- src/test/java/com/bship/integration/CreateGamesIntegrationTest.java
+++ src/test/java/com/bship/integration/CreateGamesIntegrationTest.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -25,9 +24,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @AutoConfigureMockMvc
 public class CreateGamesIntegrationTest {
-
-    @Autowired
-    private TestRestTemplate restTemplate;
 
     @Autowired
     private MockMvc mockMvc;
