d1ab6a81e19956f351e75b4747ccbe5d82fd8626
RyanDur <ryan.p.durling@gmail.com>
2017-03-16 20:16:40 -0500

    update test script

--- ci/test.sh
+++ ci/test.sh
@@ -5,10 +5,12 @@ set -e -x
 service mysql start
 
 mysql -u root -e "CREATE DATABASE $MYSQL_DATABASE;"
+mysql -u root -e "SET GLOBAL max_connections = 2048;"
 mysql -u root -e "CREATE USER 'root'@'127.0.0.1';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'127.0.0.1' WITH GRANT OPTION;"
 
 pushd bship
-    ./gradlew clean test
+    ./gradlew clean test &&
 popd
 
+service mysql stop
